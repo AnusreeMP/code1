29a768db74ed26dc88db0842766237151f64cd33
Anusree <anusreeprakash730@gmail.com>
2025-06-17 10:34:21 +0530

    second

---
+++ example.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the mark:"))
+if(a > 90 ):
+    print("A grade")
+elif(a > 80):
+    print("B grade")
+elif(a > 70):
+    print("C grade")
+elif(a > 60):
+    print("D grade")
+else:
+    print("Failed")
+   
