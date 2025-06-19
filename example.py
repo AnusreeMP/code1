74b57729f3bd24440d7e2fc24ce0c8c7785fd6f1
Anusree <anusreeprakash730@gmail.com>
2025-06-19 10:30:23 +0530

    third

--- example.py
+++ example.py
@@ -1,12 +1,14 @@
-a=int(input("Enter the mark:"))
-if(a > 90 ):
-    print("A grade")
-elif(a > 80):
-    print("B grade")
-elif(a > 70):
-    print("C grade")
-elif(a > 60):
-    print("D grade")
-else:
-    print("Failed")
-   
+# units=int(input("Enter a number of units consumed:"))
+# bill=0
+# if units <=100:
+#     bill=units*5
+# elif units <=200:
+#     bill=(100*5) + ((units -100)*8)
+# else:
+#     bill=(500+800) + ((units -200)*10)
+# print ("total water bill is",bill #while loop example:  
+count=int(input("enter the number"))
+  while count>=1:
+    if (count%2==0):
+       print(count)
+    count-=1
---
+++ loop.py
@@ -0,0 +1,8 @@
+a=0
+i=int(input("enter a number"))
+while(i!=-1):
+    a=a+i
+    i=int(input("enter a number"))
+print(a)
+        
+    
