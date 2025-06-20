d2e8b5d0569b3e63b34d7e502f671bbc538d3e1d
Anusree <anusreeprakash730@gmail.com>
2025-06-20 10:32:02 +0530

    second

--- loop.py
+++ loop.py
@@ -1,8 +1,49 @@
-a=0
-i=int(input("enter a number"))
-while(i!=-1):
-    a=a+i
-    i=int(input("enter a number"))
-print(a)
+# a=0
+# i=int(input("enter a number"))
+# while(i!=-1):
+#     a=a+i
+#     i=int(input("enter a number"))
+# print(a)
         
-    
+ #count the number of digits   
+# count=0
+# num=int(input("Enter a number:"))
+
+# while num>0:
+#     num//=10
+#     count+=1
+# print("Number of digits:",count)
+
+
+#range() function
+
+#print(range(15))
+#print(list(range(15)))
+#print(list(range(4,9)))
+#print(list(range(5,25,4)))
+
+
+# =("python","loops","sequence","condition","range")
+#for iterator in range(len(tuple_)):
+ #   print(tuple_[iterator].upper(),end=" ,")
+
+#for loop
+# numbers=[4,2,6,7,3,5,8,10,6,1,9,2]
+# square=0
+# squares=[]
+# for value in numbers:
+#     square=value**2
+#     squares.append(square)
+# print("the list of squares is",squares)
+
+#else with for loop:
+
+#string="python loop"
+#for s in string:
+   # if s=="o":
+       # print("if block")
+#else:
+     #print(s)
+
+
+     
