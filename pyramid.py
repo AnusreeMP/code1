de29e8e1fb051603ec23c2b77a69df358b0ea947
Anusree <anusreeprakash730@gmail.com>
2025-06-25 10:35:34 +0530

    third

--- pyramid.py
+++ pyramid.py
@@ -73,21 +73,80 @@
   # print()
 
 #DIAMOND Pattern
-n=5
-for i in range(0,n-1):
-  for j in range(0,n-i):
-    print("",end=" ")
+# n=4
+# for i in range(0,n-1):
+#   for j in range(0,n-i):
+#     print("",end=" ")
 
-  for k in range(0,i+1):
-     print("*",end=" ")
-  print()
-n=5
+#   for k in range(0,i+1):
+#      print("*",end=" ")
+#   print()
+# n=4
 
+# for i in range(n):
+#   for j in range(i+1):
+#       print(" ",end="")  
+#   for k in range(n-i):
+#       print("* ",end="")  
+#   print()
+
+#HOLLOW Square Pattern
+# n=5
+# for i in range(0,n):
+#   for j in range(0,n):
+#     if(i==0 or i==n-1 or j==0 or j==n-1):
+#        print("*",end=" ")
+#     else:
+#         print(" ",end=" ")
+#   print()  
+
+#FLOYD'S Triangle:
+# n=4
+# a=1
+# for i in range(n):
+#   for j in range(i+1):
+    
+#     print(a,end=" ")
+#     a+=1
+#   print()
+
+#HOURGLASS Pattern:
+# n=5
+# for i in range(n):
+#   for j in range(i+1):
+#     print(" ",end="")
+#   for k in range(0,n-i):
+#     print("* ",end="")
+#   print() 
+
+# n=5
+# for i in range(0,n):
+#   for j in range(0,n-i):
+#     print("",end=" ")
+#   for k in range(i+1):
+#     print("*",end=" ")
+#   print()  
+
+#PASCAl'S Triangle Pattern:
+# n=4
+# for i in range(n):
+#   print(" " * (n-i), end="")
+#   num=1
+#   for j in range(i+1):
+#      print(num,end=" ")
+#      num=num*(i-j)//(j+1)
+#   print()   
+
+
+#HOLLOW Full Pyramid:
+n=5
 for i in range(n):
-  for j in range(i+1):
-      print(" ",end="")  
-  for k in range(n-i):
-      print("* ",end="")  
-  print()
+  print(" " * (n-i-1),end=" ")
+    for j in range(2*i+1):
+      if j==0 or j==2*i or i==n-1:  
+       print("*",end="") 
+      else:
+        print(" ",end="") 
+   print() 
 
-
+#HOLLOW Inverted Pyramid:
