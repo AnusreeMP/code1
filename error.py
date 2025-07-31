a4507e6085f8190a90d38c4e444305c28df7ed01
Anusree <anusreeprakash730@gmail.com>
2025-07-31 10:31:52 +0530

    nine

---
+++ error.py
@@ -0,0 +1,18 @@
+# method 1:
+# x=5
+# y="Hello"
+# try:
+#     z=x+y
+# except TypeError:
+#   print("Error:cannot add an int and str")
+
+
+#Method 2:
+# x=5
+# y="Hello"
+# try: 
+#     z=x+y
+# except Exception as e:
+#   print(e)
+
+
